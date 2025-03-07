edf0535b7c921ea10186d0988dcff596f72d7743
Danard Daniels <danarddaniels@gmail.com>
2025-03-06 20:51:02 -0500

    5 StyledAbout Section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+//import global style
+import GlobalStyles from './components/GlobalStyles';
 //import pages
 import AboutUs from './pages/AboutUs';
 
@@ -6,7 +8,7 @@ import AboutUs from './pages/AboutUs';
 function App() {
   return (
     <div className="App">
-     <h1>Film!</h1>
+      <GlobalStyles />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,69 @@
 import React from 'react';
 import  home1  from '../img/home1.png';
+//Styled
+import style from 'styled-components';
+
+
 
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <StyledDescription>
                 <div className="title">
-                    <div className="hide">
+                    <StyledHide className="hide">
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide className="hide">
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </StyledHide>
+                    <StyledHide className="hide">
                         <h2>true.</h2>
-                    </div>
+                    </StyledHide>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
-                <div className="image">
-                    <img src={home1} alt="camera"/>
-                </div>
-            </div>
-        </div>
+            </StyledDescription>
+            <StyledImage className="image">
+                <img src={home1} alt="camera"/>
+            </StyledImage>
+        </StyledAbout>
     )
 };
+
+//Styled Components
+const StyledAbout = style.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const StyledDescription = style.div`
+    flex: 1;
+
+    padding-right: 5rem;
+
+    h2{
+        font-weight: lighter;
+    }
+`;
+
+const StyledImage = style.div`
+    flex: 1;
+
+    overflow: hidden;
+
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
+
+const StyledHide = style.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
