c75b3bde6a543382986127cad1232ddf3579465b
Danard Daniels <danarddaniels@gmail.com>
2025-03-16 13:50:18 -0400

    7 Styled FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //styles
 import {StyledLayout, StyledDescription, StyledImage, StyledHide} from '../styles';
+import style from 'styled-components';
 
 
 const FaqSection = () => {
     return(
-        <StyledLayout>
+        <FAQLayout>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -13,6 +14,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptates.</p>
                 </div>
+                <div className="faq-Line"></div>   
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -20,6 +22,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptates.</p>
                 </div>
+                <div className="faq-Line"></div>
             </div>
             <div className="question">
                 <h4>Different Payment Methods</h4>
@@ -27,6 +30,7 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptates.</p>
                 </div>
+                <div className="faq-Line"></div>
             </div>
             <div className="question">
                 <h4>What Products do you offer.</h4>
@@ -34,9 +38,42 @@ const FaqSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptates.</p>
                 </div>
+                <div className="faq-Line"></div>
             </div>
-        </StyledLayout>
+        </FAQLayout>
     )
 }
+
+const FAQLayout = style(StyledLayout)`
+    display: block;
+
+    span{
+        display: block;
+    }
+
+    h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+
+    .question{
+        padding: 3rem 0;
+        cursor: pointer;
+    }
+
+    .faq-Line{
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0;
+        width: 100%;
+    }
+
+    .answer{
+        padding: 2rem 0;
+        p{
+            padding: 1rem 0;
+        }
+    }
+`;
 
 export default FaqSection;
